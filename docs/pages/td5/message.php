d774eea67c179a5916b74107f62f725dc815ab31
EstelleBihel <estelle.bihel@sts-sio-caen.info>
2024-11-22 16:09:20 +0100

    '20241122'

--- docs/pages/td5/message.php
+++ docs/pages/td5/message.php
@@ -2,11 +2,11 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Affichage de Message</title>
+    <title>Afficher un message</title>
 </head>
 <body>
     <?php
-        // Vérifie si un message est passé en paramètre dans l'URL
+        // Message passé en paramètre dans l'URL?
         $message = isset($_GET['message']) ? $_GET['message'] : "Message par défaut";
 
         // Affiche le message
@@ -15,8 +15,8 @@
 
     <h3>Choisissez un message à afficher :</h3>
     <ul>
-        <li><a href="?message=Bonjour%20tout%20le%20monde">Message 1: Bonjour tout le monde</a></li>
-        <li><a href="?message=Bienvenue%20sur%20mon%20site">Message 2: Bienvenue sur mon site</a></li>
+        <li><a href="?message=Bonjour">Message 1: Bonjour</a></li>
+        <li><a href="?message=Bienvenue">Message 2: Bienvenue</a></li>
         <li><a href="?message=Merci%20de%20votre%20visite">Message 3: Merci de votre visite</a></li>
     </ul>
 </body>
